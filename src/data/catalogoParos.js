162e45cc1eb2526cc347cc7d42a2ca57720d2bee
TransmetalAG <transmetalag@gmail.com>
2026-02-06 08:12:21 -0600

    Create catalogoParos.js

---
+++ src/data/catalogoParos.js
@@ -0,0 +1,91 @@
+export const catalogoParos = {
+  "Embutidora 315": [
+    { paro: "Pérdida de presión", causa: "Mecánica" },
+    { paro: "Cambio de molde", causa: "Operacional" },
+    { paro: "Fuga de aceite", causa: "Mecánica" },
+    { paro: "Calibración de presiones", causa: "Operacional" },
+    { paro: "Calibración de sensores", causa: "Operacional" },
+    { paro: "Falla en Relé", causa: "Eléctrica" },
+    { paro: "No acciona", causa: "Mecánica" },
+    { paro: "No acciona", causa: "Eléctrica" },
+    { paro: "Falla en molde", causa: "Mecánica" }
+  ],
+
+  "Embutidora 200": [
+    { paro: "Pérdida de presión", causa: "Mecánica" },
+    { paro: "Cambio de molde", causa: "Operacional" },
+    { paro: "Fuga de aceite", causa: "Mecánica" },
+    { paro: "Calibración de presiones", causa: "Mecánica" },
+    { paro: "Calibración de sensores", causa: "Operacional" },
+    { paro: "Falla en Relé", causa: "Eléctrica" },
+    { paro: "Falla en molde", causa: "Mecánica" }
+  ],
+
+  "Perforado": [
+    { paro: "Pérdida de presión", causa: "Mecánica" },
+    { paro: "Cambio de punzones", causa: "Operacional" }
+  ],
+
+  "Tubo": [
+    { paro: "Desajuste de lanza de dobladora", causa: "Mecánica" },
+    { paro: "Desajuste de mordaza de dobladora", causa: "Mecánica" },
+    { paro: "No dobla el tubo correctamente", causa: "Mecánica" },
+    { paro: "Desajuste de sensor", causa: "Eléctrica" },
+    { paro: "Reventadura de cadena de dobladora", causa: "Mecánica" },
+    { paro: "Falta de presión", causa: "Mecánica" },
+    { paro: "No acciona la dobladora", causa: "Eléctrica" },
+    { paro: "No regresa la dobladora", causa: "Eléctrica" },
+    { paro: "Tubo golpeado o con mal ángulo", causa: "Mecánica" },
+    { paro: "Formadora no acciona", causa: "Mecánica" },
+    { paro: "Formadora no acciona", causa: "Eléctrica" },
+    { paro: "Cambio de punzones", causa: "Mecánica" },
+    { paro: "Resortes quebrados", causa: "Mecánica" },
+    { paro: "Tubo mal perforado por tope", causa: "Mecánica" },
+    { paro: "Perforadora no acciona", causa: "Mecánica" },
+    { paro: "Perforadora no acciona", causa: "Eléctrica" },
+    { paro: "Perforadora no embraga", causa: "Mecánica" },
+    { paro: "Embrague de perforadora", causa: "Mecánica" },
+    { paro: "Falla en cuña de perforadora", causa: "Mecánica" },
+    { paro: "Faja de volante reventada", causa: "Mecánica" },
+    { paro: "Falla en freno de perforadora", causa: "Mecánica" },
+    { paro: "Falla en polea de perforadora", causa: "Mecánica" },
+    { paro: "Falla en pedal de perforadora", causa: "Mecánica" },
+    { paro: "Falla en botonera de perforadora", causa: "Eléctrica" },
+    { paro: "Falla en palanca de perforadora", causa: "Mecánica" },
+    { paro: "Falla en sujetadores de molde", causa: "Mecánica" },
+    { paro: "Falla en tablero eléctrico local", causa: "Eléctrica" }
+  ],
+
+  "Serigrafía": [
+    { paro: "Problema con manta de serigrafía", causa: "Operacional" },
+    { paro: "Problema con sujetador de marco", causa: "Mecánica" },
+    { paro: "Falla de mesa giratoria", causa: "Mecánica" },
+    { paro: "Falla de molde sujetador de tolva", causa: "Mecánica" }
+  ],
+
+  "Pintura Electroestática": [
+    { paro: "Falla en quemador", causa: "Mecánica" },
+    { paro: "Motor de ventilador", causa: "Mecánica" },
+    { paro: "Arrastre de cadena", causa: "Mecánica" },
+    { paro: "Cadena de traslado de piezas", causa: "Mecánica" },
+    { paro: "Lubricación de cadena", causa: "Mecánica" },
+    { paro: "Extractores de vapores", causa: "Mecánica" },
+    { paro: "Panel principal", causa: "Eléctrica" },
+    { paro: "Panel local de ciclón", causa: "Eléctrica" },
+    { paro: "Pistolas de pintura", causa: "Mecánica" },
+    { paro: "Panel de calibración de pistolas", causa: "Eléctrica" },
+    { paro: "Sistema de carrera de pistolas", causa: "Mecánica" },
+    { paro: "Sistema de carrera de pistolas", causa: "Eléctrica" },
+    { paro: "Mangueras neumáticas", causa: "Mecánica" },
+    { paro: "Unidad de mantenimiento", causa: "Mecánica" },
+    { paro: "Tubería neumática", causa: "Mecánica" },
+    { paro: "Electroválvulas", causa: "Mecánica" },
+    { paro: "Ciclón principal", causa: "Mecánica" },
+    { paro: "Filtros de pintura", causa: "Mecánica" },
+    { paro: "Cernidores de pintura", causa: "Mecánica" },
+    { paro: "Cernidores de pintura", causa: "Eléctrica" },
+    { paro: "Cernido de pintura", causa: "Operacional" },
+    { paro: "Cerchas de materiales", causa: "Operacional" },
+    { paro: "Ganchos de cadena", causa: "Operacional" }
+  ]
+};
