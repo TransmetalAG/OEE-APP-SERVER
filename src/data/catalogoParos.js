5e945a592ff49429ce739ffbf086499d4db503d8
TransmetalAG <transmetalag@gmail.com>
2026-09-16 15:57:47 -0600

    Update catalogoParos.js

--- src/data/catalogoParos.js
+++ src/data/catalogoParos.js
@@ -152,7 +152,7 @@ export const catalogoParos = {
     { paro: "Falla en mangueras neumáticas", causa: "Mecánica" },
     { paro: "Falla en unidades de mantenimiento", causa: "Mecánica" }
   ],
-  "Troqueladora Plato": [
+  "Troqueladora de Plato": [
     { paro: "Molde en mal estado", causa: "Mecánica" },
     { paro: "Se quiebran resortes", causa: "Mecánica" },
     { paro: "Falla en sistema de embrague", causa: "Mecánica" },
